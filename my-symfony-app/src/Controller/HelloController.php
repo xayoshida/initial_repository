6c24f9376ccde75645e521fda0dbdd1bf7e7065a
xearts <xearts@mbp-13-early2011-01.local>
2020-04-04 23:25:20 +0900

    Initial Symfony
    My first Symfony

---
+++ my-symfony-app/src/Controller/HelloController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController
+{
+    public function index()
+    {
+        return new Response("Hello Symfony!");
+    }
+}
